6ac188b38459651e43a92bab7efb1fae57c1ab27
AlfieriChou <alfierichou@gmail.com>
2020-05-26 10:35:51 +0800

    chore: branchArr filter

--- index.js
+++ index.js
@@ -9,25 +9,22 @@ const deleteGitBranch = (projectPath) => {
     throw new Error('Current branch is not develop or master')
   }
   const branchArr = shell.exec('git branch -a').stdout.toString().split(/\s+/g)
-  branchArr.pop()
-  branchArr.shift()
-  _.remove(branchArr, n => {
-    return n === '*'
-  })
-  branchArr.reduce(async (promise, branch) => {
-    await promise
-    if (branch.match(/develop|master|plugin/)) {
-      return
-    }
-    if (branch.match(/origin/)) {
-      const originBranch = branch.split('remotes/')[1]
-      shell.exec(`git branch -r -d ${originBranch}`)
-      console.log('-----delete origin branch-----', originBranch)
-      return
-    }
-    shell.exec(`git branch -D ${branch}`)
-    console.log('-----delete branch-----', branch)
-  }, Promise.resolve())
+  branchArr
+    .filter(branch => !['*', '', '->'].includes(branch))
+    .reduce(async (promise, branch) => {
+      await promise
+      if (branch.match(/develop|master|plugin|HEAD/)) {
+        return
+      }
+      if (branch.match(/origin/)) {
+        const originBranch = branch.split('remotes/')[1]
+        shell.exec(`git branch -r -d ${originBranch}`)
+        console.log('-----delete origin branch-----', originBranch)
+        return
+      }
+      shell.exec(`git branch -D ${branch}`)
+      console.log('-----delete branch-----', branch)
+    }, Promise.resolve())
 }
 
 module.exports = deleteGitBranch
